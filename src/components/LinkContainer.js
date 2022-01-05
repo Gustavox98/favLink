fceabd3c2eac7c9f3da1d525c30b4ba494584a34
Gustavo <60441031+Gustavox98@users.noreply.github.com>
2022-01-05 17:54:21 -0500

    Update LinkContainer.js

--- src/components/LinkContainer.js
+++ src/components/LinkContainer.js
@@ -1,39 +1,59 @@
-import React from 'react'
-// import Table from './Table';
-// import Form from './Form';
+import React from 'react';
+ import Table from './Table';
+ import Form from './Form';
 
 class LinkContainer extends React.Component {
-  constructor(props) {
-    super(props)
-    /* TODO - Create state object for storing favLinks */
-  }
+    constructor(props){
+        super(props)
+        /* TODO - Create state object for storing favLinks */
+        this.state = {
+            favLinks : [],
+        };
+        this.removeCharacter = this.removeCharacter.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
 
-  handleRemove = (index) => {
-    /*
+    removeCharacter = index => {
+        /*
             TODO - Create logic for setting the state to filter array and remove favLink at index
         */
-  }
 
-  handleSubmit = (favLink) => {
-    /*
+            let updatedFavLinks  = this.state.favLinks.filter((fav, i) => {
+                return i !== index;
+            });
+            this.setState({
+                favLinks: updatedFavLinks,
+            });
+    };
+
+    handleSubmit = favLink => {
+        /*
             TODO - Create logic to setState and add new favLink to favLinks array in state
         */
-  }
 
-  render() {
-    return (
-      <div className="container">
-        <h1>My Favorite Links</h1>
-        <p>Add a new url with a name and link to the table.</p>
-        {/*TODO - Add Table Component */}
+            this.setState({
+                favLinks: [...this.state.favLinks, favLink]
+            });
+    }
 
+    render() {
+
+        return (
+            <div className="container">
+                <h1>My Favorite Links</h1>
+                <p>Add a new url with a name and link to the table.</p>
+                <Table
+          linkData={this.state.favLinks}
+          removeLink={this.removeCharacter}
+        />
         <br />
 
         <h3>Add New</h3>
         {/*TODO - Add Form Component */}
-      </div>
-    )
-  }
+        <Form handleSubmit={this.handleSubmit}/>
+            </div>
+        );
+    }
 }
 
-export default LinkContainer
+export default LinkContainer;
